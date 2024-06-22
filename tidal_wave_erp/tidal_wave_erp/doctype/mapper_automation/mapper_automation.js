2ca407bb53f662cc5dd39654eafdccee1dfb3556
Punam Bagal <punambagal2525@gmail.com>
2024-06-22 11:54:49 +0530

    add table mapper automation table for user

--- tidal_wave_erp/tidal_wave_erp/doctype/mapper_automation/mapper_automation.js
+++ tidal_wave_erp/tidal_wave_erp/doctype/mapper_automation/mapper_automation.js
@@ -9,8 +9,14 @@
 
 frappe.ui.form.on('Mapper Automation', {
     refresh: function(frm) {
-        // Calculate totals when the form is refreshed
+        // Calculate totals and copy data when the form is refreshed
         calculate_total_for_customer_line_items(frm);
+        copy_data_to_sales_user_table(frm);
+    },
+    before_save: function(frm) {
+        // Calculate totals and copy data before saving the form
+        calculate_total_for_customer_line_items(frm);
+        copy_data_to_sales_user_table(frm);
     }
 });
 
@@ -18,14 +24,17 @@ frappe.ui.form.on('Mapper Automation Table', {
     quantity: function(frm, cdt, cdn) {
         calculate_totals(frm, cdt, cdn);
         calculate_total_for_customer_line_items(frm);
+        copy_data_to_sales_user_table(frm);
     },
     price: function(frm, cdt, cdn) {
         calculate_totals(frm, cdt, cdn);
         calculate_total_for_customer_line_items(frm);
+        copy_data_to_sales_user_table(frm);
     },
     cost: function(frm, cdt, cdn) {
         calculate_totals(frm, cdt, cdn);
         calculate_total_for_customer_line_items(frm);
+        copy_data_to_sales_user_table(frm);
     }
 });
 
@@ -74,4 +83,27 @@ function calculate_total_for_customer_line_items(frm) {
     }
 
     frm.refresh_field('mapper_automation_table');
+}
+
+function copy_data_to_sales_user_table(frm) {
+    // Clear the sales user table
+    frm.clear_table('mapper_automation_table_for_sales_user');
+
+    // Copy data from mapper_automation_table to mapper_automation_table_for_sales_user
+    frm.doc.mapper_automation_table.forEach(row => {
+        let new_row = frm.add_child('mapper_automation_table_for_sales_user');
+        new_row.customer_line_item = row.customer_line_item;
+        new_row.customer_item_quantity = row.customer_item_quantity;
+        new_row.product_id = row.product_id;
+        new_row.quantity = row.quantity;
+        new_row.price = row.price;
+       // new_row.cost = row.cost;
+        new_row.total_price = row.total_price;
+       // new_row.total_cost = row.total_cost;
+        new_row.total_price_of_customer_line_item = row.total_price_of_customer_line_item;
+       // new_row.total_cost_of_customer_line_item = row.total_cost_of_customer_line_item;
+        // Add more fields as required
+    });
+
+    frm.refresh_field('mapper_automation_table_for_sales_user');
 }
